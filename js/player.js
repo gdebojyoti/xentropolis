25f0f5c76ae12cd83f995b74584c2608cfc9a3e7
Debojyoti Ghosh <debojyoti.ghosh@proptiger.com>
2016-12-12 20:41:49 +0530

    Intermediate commit: Player movement in progress.

---
+++ js/player.js
@@ -0,0 +1,5 @@
+"use strict";
+
+function CPlayer() {
+    this.m_cPlayer = null;
+}
--- js/utilities.js
+++ js/utilities.js
@@ -1,8 +1,14 @@
 "use strict";
 
 function registerEvents() {
+    // Viewport events
     window.addEventListener('resize', onresize);
 
+    // Keyboard events
+    document.addEventListener( 'keydown', onKeyDown, false );
+    document.addEventListener( 'keyup', onKeyUp, false );
+
+    // Mouse pointer lock events
     document.addEventListener('pointerlockchange', onpointerlockchange, false);
     document.addEventListener('mozpointerlockchange', onpointerlockchange, false);
     document.addEventListener('webkitpointerlockchange', onpointerlockchange, false);
@@ -30,3 +36,56 @@ function onpointerlockchange() {
         g_cRenderer.m_cControls.enabled = false;
     }
 }
+
+function onKeyDown(event) {
+    switch ( event.keyCode ) {
+        case 38: // up
+        case 87: // w
+            moveForward = true;
+            break;
+
+        case 37: // left
+        case 65: // a
+            moveLeft = true;
+            break;
+
+        case 40: // down
+        case 83: // s
+            moveBackward = true;
+            break;
+
+        case 39: // right
+        case 68: // d
+            moveRight = true;
+            break;
+
+        case 32: // space
+            if (canJump === true) velocity.y += 350;
+            canJump = false;
+            break;
+    }
+};
+
+function onKeyUp(event) {
+    switch( event.keyCode ) {
+        case 38: // up
+        case 87: // w
+            moveForward = false;
+            break;
+
+        case 37: // left
+        case 65: // a
+            moveLeft = false;
+            break;
+
+        case 40: // down
+        case 83: // s
+            moveBackward = false;
+            break;
+
+        case 39: // right
+        case 68: // d
+            moveRight = false;
+            break;
+    }
+};
